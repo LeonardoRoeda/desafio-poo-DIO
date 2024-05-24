cebb0e4b82697cb280703bea23c4fa4c52c949a5
LeonardoRoeda <leonardo99.roeda@gmail.com>
2024-05-24 14:30:27 -0300

    Mentoria e Curso

--- src/Mentoria.java
+++ src/Mentoria.java
@@ -1,28 +1,17 @@
 import java.time.LocalDate;
 
-public class Mentoria {
-    String titulo;
-    String descricao;
-    LocalDate data;
+public class Mentoria extends Conteudo {
+
+    private LocalDate data;
+
+    @Override
+    public double calcularXp() {
+        return XP_PADRAO + 20d;
+    }
 
     public Mentoria() {
     }
 
-    public String getTitulo() {
-        return titulo;
-    }
-
-    public void setTitulo(String titulo) {
-        this.titulo = titulo;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
 
     public LocalDate getData() {
         return data;
@@ -35,9 +24,10 @@ public class Mentoria {
     @Override
     public String toString() {
         return "Mentoria{" +
-                "titulo='" + titulo + '\'' +
-                ", descricao='" + descricao + '\'' +
+                "titulo='" + getTitulo() + '\'' +
+                ", descricao='" + getDescricao() + '\'' +
                 ", data=" + data +
                 '}';
     }
+
 }
